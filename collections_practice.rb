60a5d24e149bcbcca1c88998b4321a3133d3c437
Codi Kraus <codi.kraus@gmail.com>
2021-04-30 15:06:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,9 +47,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|word, index|}
-  if index == 1
-    word
-  else
-    word << "s"
+  array.each_with_index.collect do |word, index|
+    if index == 1
+      word
+    else
+      word << "s"
+    end
+  end
 end
