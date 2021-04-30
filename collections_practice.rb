162a3a583b2ee32031b938023b939777cf1e5384
Codi Kraus <codi.kraus@gmail.com>
2021-04-30 15:07:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,6 +21,10 @@ def swap_elements(array)
   array
 end
 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+end
+
 def reverse_array(array)
   array.reverse
 end
